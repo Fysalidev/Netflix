45fe5cf20338a473da166fd419ff550c9a40a4e6
ROBILLIARD Grégory <gregory.robilliard@icloud.com>
2023-04-25 16:26:26 +0200

    Add Google & Github btn

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -3,8 +3,14 @@ import axios from "axios";
 import { useCallback, useState } from "react";
 import Input from "@/components/Input";
 import { signIn } from "next-auth/react";
+import { useRouter } from "next/router";
+
+//icons
+import { FcGoogle } from "react-icons/fc";
+import { FaGithub } from "react-icons/fa";
 
 const Auth = () => {
+  const router = useRouter();
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -17,18 +23,6 @@ const Auth = () => {
     );
   }, []);
 
-  const register = useCallback(async () => {
-    try {
-      await axios.post("/api/register", {
-        name,
-        email,
-        password,
-      });
-    } catch (error) {
-      console.log(error);
-    }
-  }, [email, name, password]);
-
   const login = useCallback(async () => {
     try {
       await signIn("credentials", {
@@ -37,10 +31,25 @@ const Auth = () => {
         redirect: false,
         callbackUrl: "/",
       });
+
+      router.push("/");
     } catch (error) {
       console.log(error);
     }
-  }, [email, password]);
+  }, [email, password, router]);
+
+  const register = useCallback(async () => {
+    try {
+      await axios.post("/api/register", {
+        name,
+        email,
+        password,
+      });
+      login();
+    } catch (error) {
+      console.log(error);
+    }
+  }, [email, name, password, login]);
 
   return (
     <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
@@ -90,6 +99,40 @@ const Auth = () => {
             >
               {variant === "login" ? "Login" : "Sign up"}
             </button>
+            <div className="flex flex-row items-center gap-4 mt-8 justify-center">
+              <div
+                className="
+              w-10
+              h-10
+              bg-white
+              rounded-full
+              flex
+              items-center
+              justify-center
+              cursor-pointer
+              hover:opacity-80
+              transition
+              "
+              >
+                <FcGoogle size={30} />
+              </div>
+              <div
+                className="
+              w-10
+              h-10
+              bg-white
+              rounded-full
+              flex
+              items-center
+              justify-center
+              cursor-pointer
+              hover:opacity-80
+              transition
+              "
+              >
+                <FaGithub size={30} />
+              </div>
+            </div>
             <p className="text-neutral-500 mt-12">
               {variant === "login"
                 ? "First time using Netflix ?"
