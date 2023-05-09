e1416cbafb32c7e345f93db5f209cfec77c66ea6
ROBILLIARD Grégory <gregory.robilliard@icloud.com>
2023-05-09 16:10:50 +0200

    MovieCard and MovieList UI finished

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,8 @@ import { NextPageContext } from "next";
 import { getSession } from "next-auth/react";
 import Navbar from "@/components/Navbar";
 import Billboard from "@/components/Billboard";
+import MovieList from "@/components/MovieList";
+import useMovieList  from "@/hooks/useMovieList";
 
 // Rediriger vers la page d'authentification si l'utilisateur n'est pas connecté
 export async function getServerSideProps(context: NextPageContext) {
@@ -23,10 +25,15 @@ export async function getServerSideProps(context: NextPageContext) {
 
 // Home page UI
 export default function Home() {
+  const {data: movies = []} =  useMovieList()
+
   return (
     <>
       <Navbar />
       <Billboard />
+      <div className="pb-40">
+        <MovieList title="Trending Now" data={movies} />
+      </div>
     </>
   );
 }
